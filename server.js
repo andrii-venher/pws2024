86c18b5f1e5ee70e3d3407a9444f491ce518d997
Mariusz Jarocki <mariusz.jarocki@wmii.uni.lodz.pl>
2024-12-16 14:43:31 +0100

    Projects in the backend

--- server.js
+++ server.js
@@ -58,14 +58,14 @@ const personSchema = new mongoose.Schema({
     _id: { type: String, default: uuid.v4 },
     firstName: { type: String, required: true, validate: {
         validator: v => {
-          return /^\p{L}/u.test(v);
+          return /^\p{L}/u.test(v)
         },
         message: props => `${props.value} does not start from a letter`
       }
     },
     lastName: { type: String, required: true, validate: {
         validator: v => {
-          return /^\p{L}/u.test(v);
+          return /^\p{L}/u.test(v)
         },
         message: props => `${props.value} does not start from a letter`
       }
@@ -96,8 +96,6 @@ app.get(personEndpoint, (req, res) => {
 
     const aggregation = [ matching ]
 
-    aggregation.push({ $match: { firstName: { $regex: req.query.firstName || '' }}})
-    aggregation.push({ $match: { lastName: { $regex: req.query.lastName || '' }}})
     if(req.query.sort) {
         aggregation.push({ $sort: sort })
     }
@@ -146,7 +144,6 @@ app.put(personEndpoint, (req, res) => {
     delete req.body._id
     Person.findOneAndUpdate({ _id }, { $set: req.body }, { new: true, runValidators: true })
         .then(row => {
-            console.log(row)
             res.json(row)
         })
         .catch(err => {
@@ -169,10 +166,112 @@ app.delete(personEndpoint, (req, res) => {
         })
 })
 
+const projectSchema = new mongoose.Schema({
+    _id: { type: String, default: uuid.v4 },
+    name: { type: String, required: true, validate: {
+        validator: v => {
+          return /^\p{L}/u.test(v)
+        },
+        message: props => `${props.value} does not start from a letter`
+      }
+    },
+    startDate: { type: Date, required: true, transform: v => v.toISOString().substr(0, 10) },
+    endDate: { type: Date, required: false, transform: v => v.toISOString().substr(0, 10) }
+}, {
+    versionKey: false,
+    additionalProperties: false
+})
+
+let Project = null
+
+const projectEndpoint = '/api/project'
+
+app.get(projectEndpoint, (req, res) => {
+    let sort = {}
+    if(req.query.sort) {
+        sort[req.query.sort] = +req.query.order || 1
+    }
+    const matching = {
+        $match: { name: { $regex: req.query.search || '', $options: 'i' }},
+    }
+
+    const aggregation = [ matching ]
+
+    if(req.query.sort) {
+        aggregation.push({ $sort: sort })
+    }
+    aggregation.push({ $skip: +req.query.skip || 0 })
+    let limit = +req.query.limit
+    if(!isNaN(limit) && limit > 0) {
+        aggregation.push({ $limit: limit })
+    }
+    Project.aggregate([{ $facet: {
+        total: [ matching, { $count: 'count' } ],
+        data: aggregation
+    }}])
+    .then(facet => {
+        [ facet ] = facet
+        facet.total = ( facet.total && facet.total[0] ? facet.total[0].count : 0) || 0
+        facet.data = facet.data.map(project => new Project(project))
+        res.json(facet)
+    })
+    .catch(err => {
+        res.status(400).json({ error: err.message })
+    })  
+})
+
+app.post(projectEndpoint, (req, res) => {
+    let project = new Project(req.body)
+    let err = project.validateSync()
+    if(err) {
+        res.status(400).json({ error: err.message })
+        return    
+    }
+    project.save()
+        .then(row => {
+            res.json(row)
+        })
+        .catch(err => {
+            res.status(400).json({ error: err.message })
+        })
+})
+
+app.put(projectEndpoint, (req, res) => {
+    let _id = req.body._id
+    if(!_id) {
+        res.status(400).json({ error: 'no _id!' })
+        return
+    }
+    delete req.body._id
+    Project.findOneAndUpdate({ _id }, { $set: req.body }, { new: true, runValidators: true })
+        .then(row => {
+            res.json(row)
+        })
+        .catch(err => {
+            res.status(400).json({ error: err.message })
+        })
+})
+
+app.delete(projectEndpoint, (req, res) => {
+    let _id = req.query._id
+    if(!_id) {
+        res.status(400).json({ error: 'no _id!' })
+        return
+    }
+    Project.findOneAndDelete({ _id })
+        .then(row => {
+            res.json(row)
+        })
+        .catch(err => {
+            res.status(400).json({ error: err.message })
+        })
+})
+
 mongoose.connect(config.dbUrl)
 .then(conn => {
     console.log(`Connection to ${config.dbUrl} established`)
     Person = conn.model('Person', personSchema)
+    Project = conn.model('Project', projectSchema)
 
     app.listen(config.port, () => {
         console.log('Backend listening on port', config.port)
