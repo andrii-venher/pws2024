dd19ce2e5b5ec7e4a246b71fa773fba91551f681
Andrii Venher <vengerandr@gmail.com>
2025-01-31 21:25:37 +0100

    Format files

--- pws2024-vue/src/mixins/common.js
+++ pws2024-vue/src/mixins/common.js
@@ -1,13 +1,13 @@
 const getIntersection = (array1, array2) => {
-    const lookupSet = new Set(array2)
-    return array1.filter(element => lookupSet.has(element))
-}
+  const lookupSet = new Set(array2);
+  return array1.filter((element) => lookupSet.has(element));
+};
 
 export default {
-    methods: {
-        checkIfInRole(session, roles) {
-            let intersection = getIntersection(session.roles || [], roles || [])
-            return intersection.length > 0
-        }
-    }
-}
+  methods: {
+    checkIfInRole(session, roles) {
+      let intersection = getIntersection(session.roles || [], roles || []);
+      return intersection.length > 0;
+    },
+  },
+};
--- pws2024-vue/vite.config.js
+++ pws2024-vue/vite.config.js
@@ -1,26 +1,24 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
   server: {
     proxy: {
-      '/api': 'http://localhost:8000',
-      '/ws': {
-        target: 'ws://localhost:8000',
+      "/api": "http://localhost:8000",
+      "/ws": {
+        target: "ws://localhost:8000",
         ws: true,
-        changeOrigin: true
-      }
-    }
-  }
-})
+        changeOrigin: true,
+      },
+    },
+  },
+});
